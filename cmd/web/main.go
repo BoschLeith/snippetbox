31744e0d26f32fa8888cade67d9b5cf6a8ff2a6b
BoschLeith <bosch.leith@gmail.com>
2024-09-17 18:58:27 -0500

    feat: add caching templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/BoschLeith/snippetbox/internal/models"
+	"github.com/joho/godotenv"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 type application struct {
-	logger   *slog.Logger
-	snippets *models.SnippetModel
+	logger        *slog.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -38,12 +40,18 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
-
 	defer db.Close()
 
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
 	app := &application{
-		logger:   logger,
-		snippets: &models.SnippetModel{DB: db},
+		logger:        logger,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	logger.Info("starting server", slog.String("addr", *addr))
